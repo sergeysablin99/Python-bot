9893e6d0c87f68255acf1d3864e1d51c8c30d69c
exeynod <exeynod@gmail.com>
2020-01-27 17:01:46 +0300

    implemented user class and users dict

--- Python-bot/server.py
+++ Python-bot/server.py
@@ -2,10 +2,11 @@ import requests
 import json
 import random
 
+
 class Server:
     group_id = 191177272
     access_token = 'access_token=f068c796542cba0f4dbdd0f6e39ba656a489731d36cfdcbdf7cee30de822ae000aa9e1aa8293bc61d77c7'
-    v = 'v=5.103'    # Текущая версия VkAPI
+    v = 'v=5.103'  # Текущая версия VkAPI
     body = 'https://api.vk.com/method/'  # Тело запроса
 
     def __init__(self):
@@ -32,7 +33,8 @@ class Server:
             message = reply['updates'][0]['object']['message']['text']
             user_id = reply['updates'][0]['object']['message']['from_id']
             random_id = random.randint(0, 100)
-            method = 'messages.send?' + 'user_id=' + str(user_id) + '&random_id=' + str(random_id) + '&message=' + (message)
+            method = 'messages.send?' + 'user_id=' + str(user_id) + '&random_id=' + str(random_id) \
+                     + '&message=' + message
             r = requests.get("&".join([Server.body + method, Server.v, Server.access_token]))
             print("&".join([Server.body + method, Server.v, Server.access_token]))
         self.simple_loop()
